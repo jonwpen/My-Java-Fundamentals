28c69380885024c37ecd16a559124d2c8d14064c
John Pennington <johnpennington@Johns-iMac.local>
2023-06-29 08:34:43 -0500

    Finished Queue labs

--- src/labs_examples/datastructures/queue/labs/Exercise_01.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_01.java
@@ -1,5 +1,8 @@
 package labs_examples.datastructures.queue.labs;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *      Demonstrate your mastery of Java's (sun's) built-in Queue class (as seen in the import statement
  *      above) by completing the following:
@@ -7,3 +10,24 @@ package labs_examples.datastructures.queue.labs;
  *      1) instantiate a new Queue
  *      2) demonstrate the use of EVERY method in the Queue class - there are 7
  */
+public class Exercise_01{
+	public static void main(String[] args) {
+		
+		Queue<Integer> q = new PriorityQueue<>();
+		
+		q.add(7);
+		q.add(6);
+		q.add(5);
+		q.add(4);
+		q.add(3);
+		q.add(2);
+		q.add(1);
+		q.offer(77);
+		System.out.println(q.remove());
+		System.out.println(q.poll());
+		System.out.println(q.element());
+		System.out.println(q.peek());
+		System.out.println(q.size());
+		
+	}
+}
--- src/labs_examples/datastructures/queue/labs/Exercise_02.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_02.java
@@ -1,5 +1,9 @@
 package labs_examples.datastructures.queue.labs;
 
+import java.util.Arrays;
+
+
+
 /**
  *      Queues - Exercise_02
  *
@@ -16,3 +20,158 @@ package labs_examples.datastructures.queue.labs;
  *      6) contain a method to print out the data of all elements in the Queue
  *
  */
+//This exercise matches the Stack exercise, but adding and removing now enforce FIFO architecture.
+public class Exercise_02{
+	public static void main(String[] args) {
+		
+		CustomQueue<Integer> intQueue = new CustomQueue<>();
+
+		try {
+			intQueue.remove();
+		} catch (EmptyQueueException e) {
+			e.printStackTrace();
+		}
+		System.out.println(intQueue.peekFirst());
+		intQueue.add(3);
+		intQueue.add(6);
+		intQueue.add(9);
+		intQueue.add(12);
+		intQueue.add(15);
+		intQueue.add(18);
+		intQueue.add(21);
+		intQueue.add(24);
+		intQueue.add(27);
+		intQueue.add(30);
+		intQueue.add(33);
+		intQueue.add(36);
+		System.out.println(intQueue.toString());
+		try {
+			System.out.println(intQueue.remove());
+		} catch (EmptyQueueException e) {
+			e.printStackTrace();
+		}
+		System.out.println(intQueue.toString());
+		try {
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+			System.out.println(intQueue.remove());
+		} catch (EmptyQueueException e) {
+			e.printStackTrace();
+		}
+		System.out.println(intQueue.toString());
+	}
+}
+class CustomQueue<T>{
+	
+	private T[] data;
+	private int size;
+	
+	@SuppressWarnings("unchecked")
+	CustomQueue(){
+		data = (T[]) new Object[10];
+		size = 0;
+	}
+	
+	public void add(T element) {
+		if(data[0] == null) {
+			data[0] = element;
+			size++;
+		}
+		else {
+			for(int i = 0; i < data.length; i++) {
+				//add an element to the next null index starting from the front. Call resize()
+				if(data[i] == null){
+					data[i] = element;
+					size++;
+					break;
+				}
+			}
+		}
+		resize();
+	}
+	//Remove method enforces FIFO rule. Elements are added left to right. Elements are removed left to right.
+	public T remove() throws EmptyQueueException{
+		T element = data[0];
+		//if the first element is null, throw a custom exception
+		if(data[0] == null) {
+			throw new EmptyQueueException(" ");
+		}
+		//remove the 0 index and shift elements to the left. Call resize()
+		for(int i = 0;i < data.length-1;i++) {
+			data[i] = data[i+1];
+			}
+		resize();
+		size--;
+		return element;
+	}
+	
+	public int size() {
+		return size;
+	}
+	@Override
+	public String toString() {
+		//iterate over the elements including null values
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0;i<data.length;i++) {
+			sb.append(data[i]).append(" ");
+		}
+		return sb.toString();
+	}
+	
+	public String toStringNotNull() {
+		//iterate only over elements that contain values
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0;i<data.length;i++) {
+			if(data[i] != null) {
+				sb.append(data[i]).append(" ");
+			}
+		}
+		return sb.toString();
+	}
+	public T peekFirst() {
+		//"First" being the bottom of the Queue
+		T x = null;
+		for(int i = 0;i<data.length;i++){
+			if(data[i] == null && size > 1) {
+				x = data[i-1];
+				break;
+			}
+			else {
+				x = data[0];
+			}
+		}
+		return x;
+	}
+	public T peekLast() {
+		//"Last" being the bottom of the Queue
+		return data[0];
+	}
+	public void resize() {
+		//if more than 75% full, double size
+		if(size >= (data.length / 4) * 3) {
+			T[] newData = Arrays.copyOf(data,data.length * 2);
+			data = newData;
+		}
+		//if less than 25% full AND overall length is greater than the initial size of 10, halve the size
+		else if(size <= data.length / 4 && data.length > 10) {
+			T[] newData = Arrays.copyOf(data,data.length / 2);
+			data = newData;
+		}
+	}
+}
+
+class EmptyQueueException extends Exception{
+	@Override
+	public String toString() {
+		return "EmptyQueueException: Cannot remove element from an empty queue.";
+	}
+	public EmptyQueueException(String string) {
+	}
+	private static final long serialVersionUID = 1L;
+}
